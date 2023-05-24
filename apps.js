1ac162012785e6bdd2431b6e11f45fec477a1c25
eltonPillay <biduudywebsolutions@gmail.com>
2023-05-24 20:16:36 +0200

    got app to work but not yet styles

---
+++ apps.js
@@ -0,0 +1,27 @@
+const but = document.getElementById('genbut');
+const question = document.getElementById('jokeq');
+const answer = document.getElementById('jokea');
+
+
+
+
+
+
+but.addEventListener('click', async (event) => {
+  event.preventDefault();
+  try {
+    const joke = await axios.get("https://joke.deno.dev/");
+    question.innerHTML = joke.data.setup;
+    answer.innerHTML = joke.data.punchline;
+  } catch {
+    console.log("not working!!!")
+  }
+});
+
+
+
+//console.log(joke.data.setup);
+//console.log(joke.data.punchline);
+
+
+
